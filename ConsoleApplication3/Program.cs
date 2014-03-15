f7bdb903af78ca05410ccc7169fb197d1a11c7cd
Maor Hayun <maor.hayun@gmail.com>
2014-03-15 17:44:11 +0200

    intro commit

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = new string("sfdf".ToCharArray());
+
+            FileSystemWatcher fsw = new FileSystemWatcher();
+        }
+    }
+}
